5eee073142cd7e6cb90c22e0585a6e5fa026df3f
young_og <jessieshukeireh2001@gmail.com>
2022-02-22 08:53:05 -0500

    First Commit

---
+++ Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace RPG.Combat
+{
+  public class Fighter : MonoBehaviour
+  {
+    public void Attack()
+    {
+      print("Take that you short, squat peasant");
+    }
+  }
+}
---
+++ Assets/Scripts/Control/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Movement;
+using System;
+
+namespace RPG.Control
+{
+  public class PlayerController : MonoBehaviour
+  {
+    private void Update()
+    {
+      InteractWithCombat();
+      InteractWithMovement();
+    }
+
+    private void InteractWithCombat()
+    {
+      throw new NotImplementedException();
+    }
+
+    private void InteractWithMovement()
+    {
+      if (Input.GetMouseButton(0))
+      {
+        MoveToCursor();
+      }
+    }
+
+    private void MoveToCursor()
+    {
+      Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+      RaycastHit hit;
+      bool hasHit = Physics.Raycast(ray, out hit);
+      if (hasHit)
+      {
+        GetComponent<Mover>().MoveTo(hit.point);
+      }
+    }
+  }
+}
